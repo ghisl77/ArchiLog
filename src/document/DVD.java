e4dc9fbe9c1fddcd6e412af08b9f183cbbf36df3
ghisl77 <129007773+ghisl77@users.noreply.github.com>
2024-03-30 15:45:05 +0100

    age

--- src/document/DVD.java
+++ src/document/DVD.java
@@ -1,9 +1,26 @@
 package document;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class DVD extends Document{
     private boolean adulte;
     public DVD(int num, String titre,boolean adulte){
         super(num,titre);
         this.adulte = adulte;
+    }
+    public boolean verifieAge(Date dateNais){
+        if(adulte==true){
+            Date currentDate = new Date();
+            long diffInMillis = currentDate.getTime() - dateNais.getTime();
+            long ageInMillis = diffInMillis / (1000L * 60 * 60 * 24 * 365);
+            int age = (int) ageInMillis;
+            if(age>16){
+                return true;
+            }
+            else
+                return false;
+        }
+        return true;
     }
 }
